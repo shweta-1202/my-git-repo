27b7536875b66ea7638ecbf231b3dd9438d93185
Your Name <you@example.com>
2024-06-08 12:30:02 +0530

    added a new file

---
+++ hello.py
@@ -0,0 +1,20 @@
+list = [1,2,5,7,'yes']
+
+a = 9
+b = 9.05
+c = 'ok'
+
+print(type(list))
+print(type(a))
+print(type(b))
+print(type(c))
+
+x = {1,2,3,4,5,"krutika",7,8,9,20}
+print(x)
+
+school = {
+    'stud_name':'Krutika',
+    'stud_no':92
+}
+print(school)
+print(type(school))
